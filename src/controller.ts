ed59fb7fcb53f3f57182094f9f6487a891653fb4
oh-jinsu <ohjinsu98@icloud.com>
2026-02-27 18:02:53 +0900

    Support for single default value

--- src/controller.ts
+++ src/controller.ts
@@ -1,173 +1,176 @@
 import { base64Uploader } from "./base64_uploader";
 import { generateSizeMetadata } from "./metadata";
 
-export type FileSnapshot<TFile> = {
-  uniqueKey: string;
-  aspectRatio: number | undefined;
-  isLoading: boolean;
-  size: number | undefined;
-  type: string | undefined;
-  name: string | undefined;
-  width: number | undefined;
-  height: number | undefined;
-  thumbnail: string | undefined;
-  file: TFile;
+export type FileMetadata = {
+    size: number | undefined;
+    type: string | undefined;
+    name: string | undefined;
+    width: number | undefined;
+    height: number | undefined;
+    thumbnail: string | undefined;
 };
 
+export type FileSnapshot<TFile> = {
+    uniqueKey: string;
+    aspectRatio: number | undefined;
+    isLoading: boolean;
+    file: TFile;
+} & FileMetadata;
+
 export type FileUploader<TFile> = (
-  file: File
+    file: File,
 ) => Awaited<TFile> | Promise<Awaited<TFile>>;
 
 export type FileInputSubscriber = () => void;
 
-export type FileInputControllerOptions<TFile> = {
-  uploader?: FileUploader<TFile>;
-  defaultValue?: {
-    size?: number;
-    width?: number;
-    height?: number;
-    type?: string;
-    name?: string;
-    thumbnail?: string;
+export type DefaultValueOf<TFile> = {
     file: TFile;
-  }[];
-  multiple?: boolean;
+} & Partial<FileMetadata>;
+
+export type FileInputControllerOptions<TFile> = {
+    uploader?: FileUploader<TFile>;
+    defaultValue?: DefaultValueOf<TFile>[] | DefaultValueOf<TFile>;
+    multiple?: boolean;
 };
 
 export class FileInputController<TFile> {
-  protected _isDragOver = false;
+    protected _isDragOver = false;
 
-  get isDragOver() {
-    return this._isDragOver;
-  }
-
-  set isDragOver(value: boolean) {
-    const isUpdated = this._isDragOver !== value;
-
-    this._isDragOver = value;
-
-    if (isUpdated) {
-      this.notify();
+    get isDragOver() {
+        return this._isDragOver;
     }
-  }
 
-  protected _snapshots: FileSnapshot<TFile>[] = [];
+    set isDragOver(value: boolean) {
+        const isUpdated = this._isDragOver !== value;
 
-  get snapshots() {
-    return [...this._snapshots];
-  }
+        this._isDragOver = value;
 
-  set snapshots(value: FileSnapshot<TFile>[]) {
-    this._snapshots = value;
-    this.notify();
-  }
-
-  get uploadedFiles() {
-    return this._snapshots
-      .filter((snapshot) => !snapshot.isLoading)
-      .map((snapshot) => snapshot.file);
-  }
-
-  protected subscribers: FileInputSubscriber[] = [];
-
-  protected uploader: FileUploader<TFile>;
-
-  protected multiple: boolean;
-
-  constructor({
-    uploader,
-    defaultValue,
-    multiple = true,
-  }: FileInputControllerOptions<TFile> = {}) {
-    this.multiple = multiple;
-
-    this.uploader = uploader || (base64Uploader as FileUploader<TFile>);
-
-    this._snapshots =
-      defaultValue?.map(
-        ({ width, height, size, type, name, file, thumbnail }) => ({
-          uniqueKey: crypto.randomUUID(),
-          aspectRatio: width && height ? width / height : undefined,
-          isLoading: false,
-          name,
-          size,
-          type,
-          width,
-          height,
-          thumbnail,
-          file,
-        })
-      ) || [];
-  }
-
-  subscribe(subscriber: () => void) {
-    this.subscribers.push(subscriber);
-
-    return () => {
-      const index = this.subscribers.indexOf(subscriber);
-      if (index !== -1) {
-        this.subscribers.splice(index, 1);
-      }
-    };
-  }
-
-  async upload(files: File[]): Promise<void> {
-    for (const file of files) {
-      const { type, name, size } = file;
-
-      const { width, height, thumbnail } = await generateSizeMetadata(file);
-
-      const aspectRatio = width && height ? width / height : undefined;
-
-      const uniqueKey = crypto.randomUUID();
-
-      const snapshot1 = {
-        uniqueKey,
-        isLoading: true,
-        size,
-        type,
-        name,
-        width,
-        height,
-        aspectRatio,
-        thumbnail,
-        file: {} as TFile,
-      };
-
-      this.snapshots = this.multiple
-        ? [...this.snapshots, snapshot1]
-        : [snapshot1];
-
-      const uploadedFile = await this.uploader(file);
-
-      const snapshot2 = {
-        uniqueKey,
-        isLoading: false,
-        size,
-        type,
-        name,
-        width,
-        height,
-        aspectRatio,
-        thumbnail,
-        file: uploadedFile,
-      };
-
-      this.snapshots = this.snapshots.map((snapshot) =>
-        snapshot.uniqueKey === uniqueKey ? snapshot2 : snapshot
-      );
+        if (isUpdated) {
+            this.notify();
+        }
     }
-  }
 
-  remove(snapshot: FileSnapshot<TFile>) {
-    this.snapshots = this.snapshots.filter(
-      (s) => s.uniqueKey !== snapshot.uniqueKey
-    );
-  }
+    protected _snapshots: FileSnapshot<TFile>[] = [];
 
-  notify() {
-    for (const subscriber of this.subscribers) {
-      subscriber();
+    get snapshots() {
+        return [...this._snapshots];
     }
-  }
+
+    set snapshots(value: FileSnapshot<TFile>[]) {
+        this._snapshots = value;
+        this.notify();
+    }
+
+    get uploadedFiles() {
+        return this._snapshots
+            .filter((snapshot) => !snapshot.isLoading)
+            .map((snapshot) => snapshot.file);
+    }
+
+    protected subscribers: FileInputSubscriber[] = [];
+
+    protected uploader: FileUploader<TFile>;
+
+    protected multiple: boolean;
+
+    constructor({
+        uploader,
+        defaultValue,
+        multiple = true,
+    }: FileInputControllerOptions<TFile> = {}) {
+        this.multiple = multiple;
+
+        this.uploader = uploader || (base64Uploader as FileUploader<TFile>);
+
+        this._snapshots =
+            (Array.isArray(defaultValue)
+                ? defaultValue
+                : defaultValue
+                  ? [defaultValue]
+                  : []
+            )?.map(({ width, height, size, type, name, file, thumbnail }) => ({
+                uniqueKey: crypto.randomUUID(),
+                aspectRatio: width && height ? width / height : undefined,
+                isLoading: false,
+                name,
+                size,
+                type,
+                width,
+                height,
+                thumbnail,
+                file,
+            })) || [];
+    }
+
+    subscribe(subscriber: () => void) {
+        this.subscribers.push(subscriber);
+
+        return () => {
+            const index = this.subscribers.indexOf(subscriber);
+            if (index !== -1) {
+                this.subscribers.splice(index, 1);
+            }
+        };
+    }
+
+    async upload(files: File[]): Promise<void> {
+        for (const file of files) {
+            const { type, name, size } = file;
+
+            const { width, height, thumbnail } =
+                await generateSizeMetadata(file);
+
+            const aspectRatio = width && height ? width / height : undefined;
+
+            const uniqueKey = crypto.randomUUID();
+
+            const snapshot1 = {
+                uniqueKey,
+                isLoading: true,
+                size,
+                type,
+                name,
+                width,
+                height,
+                aspectRatio,
+                thumbnail,
+                file: {} as TFile,
+            };
+
+            this.snapshots = this.multiple
+                ? [...this.snapshots, snapshot1]
+                : [snapshot1];
+
+            const uploadedFile = await this.uploader(file);
+
+            const snapshot2 = {
+                uniqueKey,
+                isLoading: false,
+                size,
+                type,
+                name,
+                width,
+                height,
+                aspectRatio,
+                thumbnail,
+                file: uploadedFile,
+            };
+
+            this.snapshots = this.snapshots.map((snapshot) =>
+                snapshot.uniqueKey === uniqueKey ? snapshot2 : snapshot,
+            );
+        }
+    }
+
+    remove(snapshot: FileSnapshot<TFile>) {
+        this.snapshots = this.snapshots.filter(
+            (s) => s.uniqueKey !== snapshot.uniqueKey,
+        );
+    }
+
+    notify() {
+        for (const subscriber of this.subscribers) {
+            subscriber();
+        }
+    }
 }
